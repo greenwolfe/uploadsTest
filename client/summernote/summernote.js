a0b2ab6661ff802c3a15c7c0f9c9ef0b901bdf0d
Matt Greenwolfe <solargreen@mindspring.com>
2015-04-28 18:22:08 -0400

    getting close

--- client/summernote/summernote.js
+++ client/summernote/summernote.js
@@ -39,26 +39,17 @@ Template.summernote.onRendered(function() {
   var saveText = function(event) { //called on popover blur event
     var elementID = event[0].target.id;
     var popoverVisible = false;
-    ['#note-popover-','#note-handle-','#note-dialog-'].forEach(function(s) {
+    ['#note-popover-','#note-dialog-'].forEach(function(s) {
       var selector = elementID.replace('note-editor-',s);
-      console.log(selector);
-      if ($(selector).is(':visible')) {
-        console.log(selector + ' is visible');
-        popoverVisible = true;
-        return;
-      }
+      _.each($(selector).children(),function(c) {
+        if ($(c).is(':visible')) {
+          console.log(selector + ' ' + c.id + ' is visible');
+          popoverVisible = true
+          return;
+        }
+      })
     });
     if (popoverVisible) return;
-/*   if ($(popoverSelector).is(':visible'))
-      console.log('dialog visible');
-    var popover = $(popoverSelector);
-    _.each(popover.children(),function(child) {
-      console.log(child.id);
-      console.log($(child).is(':visible'));
-    })
-    popoverSelector += ' .note-air-popover';
-    if ($(popoverSelector).is(':visible')) //clicking on popover menus cauases a false blur event
-      return;*/
     console.log('popover not visible, so going ahead'); 
     var item = {
       _id: data._id
@@ -75,24 +66,22 @@ Template.summernote.onRendered(function() {
   //and make sure popover is closed and save is called
   // ... second-order correction to false blur handler in save function
   $(document).click(function(event) { 
-    var id = element.attr('id') || '';
-    //console.log('checking' + id);
-    if ($(event.target).parent().attr('id') == id) return; //clicked in edited element
-    //onsole.log('did not click on ' + id);
-    var popoverSelector = id.replace('note-editor-','#note-popover-'); //clicked inside associated air popover
-    var popover = $(popoverSelector + 'note-air-popover'); //just the main menu part
-    //console.log(popover);
-    if (!popover.is(':visible')) return; //popover not open, no need to close it
-    //console.log(popoverSelector + ' is open.');
-    if($(event.target).closest(popoverSelector).length) return;  //clicked outside open air popover
-    //console.log('did not click inside popover ' + popoverSelector);
-    var handleSelector = id.replace('note-editor-','#note-handle-');
-    if($(event.target).closest(handleSelector).length) return;
-    //console.log('did not click a popover handle ' + handleSelector);
-    var dialogSelector = id.replace('note-editor-','#note-dialog-');
-    if($(event.target).closest(dialogSelector).length) return;
-    //console.log('did not click air editor dialog ' + dialogSelector);
-    console.log('clicked entirely outside this active (popover open) summernote, hiding and saving ' + popoverSelector)
+    var elementID = element.attr('id') || '';
+    if ($(event.target).parent().attr('id') == elementID) return; //clicked in edited element
+    var popoverVisible = false;
+    ['#note-popover-','#note-dialog-'].forEach(function(s) {
+      var selector = elementID.replace('note-editor-',s);
+      _.each($(selector).children(),function(c) {
+        if ($(c).is(':visible')) {
+          console.log(selector + ' ' + c.id + ' is visible');
+          popoverVisible = true
+          return;
+        }
+      })
+    });
+    if (!popoverVisible) return;    
+
+    var popoverSelector = elementID.replace('note-editor-','#note-popover-'); 
     popover = $(popoverSelector); //getting container for link, image, air
     popover.children().hide(); //clicked outside all associated elements and popovers ... hide it
     saveText([{target:{id:'notAnElement'}}]); //and save any changes
@@ -113,11 +102,11 @@ Template.summernote.onRendered(function() {
       //console.log('');
       element.summernote(options);
     } else {
-      console.log('destroying summernote');
-      console.log(element);
+      //console.log('destroying summernote');
+      //console.log(element);
       element.destroy();
-      console.log(element);
-      console.log('');
+      //console.log(element);
+      //console.log('');
     }
     //add handler to reactively change other options
     //as I did for sortable?
